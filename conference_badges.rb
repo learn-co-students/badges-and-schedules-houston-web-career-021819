77d2c59b84eb990a40d49a5e4a2a9d522aefc83e
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 22:06:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+def badge_maker(name)
+  "Hello, my name is #{name.capitalize}."
+end
+#badge_maker("jason")
+
+def batch_badge_creator(name)
+  name.collect {|name|  "Hello, my name is #{name}."}
+end
+#batch_badge_creator(names)
+
+def assign_rooms(name)
+  name.collect.with_index(1) do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+#assign_rooms(names)
+
+def printer(names)
+  batch_badge_creator(names).each do |name|
+    puts name
+  end
+  assign_rooms(names).each do |room|
+    puts  room
+  end
+end
+
+printer(names)
